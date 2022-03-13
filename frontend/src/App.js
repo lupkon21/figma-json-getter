ac475240dbe0ef789f91722e4dd622bff3edd7ce
Lukas Protiva <luky.protiva@gmail.com>
2022-03-13 13:42:37 +0100

    add axios GET request function

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,22 @@
 import "./App.css";
+import React from "react";
+import Axios from "axios";
 
 function App() {
-    return <div className="App"></div>;
+    const getJson = () =>
+        Axios.get("http://localhost:8080/json")
+            .then((response) => {
+                console.log(response.data);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+
+    return (
+        <div className="App">
+            <input type="button" value="GET JSON" onClick={getJson} />
+        </div>
+    );
 }
 
 export default App;
