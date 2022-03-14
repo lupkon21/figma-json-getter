6270f2cb5fcde40be8d0320e6e3541ecd7d8acfe
Lukas Protiva <luky.protiva@gmail.com>
2022-03-14 21:54:13 +0100

    add other form components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import React from "react";
 import Axios from "axios";
+import Logo from "./graphics/figma_logo.png";
 
 function App() {
     const getJson = () =>
@@ -15,7 +16,41 @@ function App() {
     return (
         <div className="App">
             <form className="Form-JSON">
-                <input type="button" value="GET JSON" onClick={getJson} />
+                <div className="Form-left">
+                    <img className="Form-logo" src={Logo} alt="figma_logo" />
+                    {/*<input type="button" value="GET JSON" onClick={getJson} />*/}
+                </div>
+
+                <div className="Form-right">
+                    <h1 className="Form-heading">FIGMA JSON GETTER</h1>
+                    <input
+                        type="text"
+                        name="figma_token"
+                        placeholder="xxxxx-xxxxx-xxxxx"
+                        className="Form-input-field"
+                    />
+                    <br />
+                    <input
+                        type="text"
+                        name="file_id"
+                        placeholder="xxxx"
+                        className="Form-input-field"
+                    />
+                    <br />
+                    <input
+                        type="reset"
+                        name="reset"
+                        value="RESET"
+                        className="Form-input-reset Form-input-btn"
+                    />
+                    <input
+                        type="button"
+                        name="submit"
+                        value="SUBMIT"
+                        className="Form-input-submit Form-input-btn"
+                        onClick={getJson}
+                    />
+                </div>
             </form>
         </div>
     );
